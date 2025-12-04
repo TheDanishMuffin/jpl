086f9e419f4df8a9b1f0a4b74d3bed0eba6798e5
Ken1707 <kenzo.chinchilla@bhcc.edu>
2025-12-04 04:38:01 -0500

    4amUpdate fourMotors.java

--- fourMotors.java
+++ fourMotors.java
@@ -22,14 +22,14 @@ public class fourMotors extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        int UP_POSITION = -500; // og -545
-        int DOWN_POSITION = 50;
+        int UP_POSITION = -545; // og -545
+        int DOWN_POSITION = 30; //50
         int down_hold = 1800;
         
         double ratio_offset = 1.5;
 
         // 0 to 2940
-        double LIFT_VEL = 900; // og 600, max 1600 or else the motors will stall / twist. 300 for testing w/o weights
+        double LIFT_VEL = 1500; // og 600, max 1600 or else the motors will stall / twist. 300 for testing w/o weights
         double LOWER_VEL = 350;
         
         // motor init stuff. the 0 and 3 motors are 40:1, 1 and 2 are 60:1
@@ -83,7 +83,7 @@ public class fourMotors extends LinearOpMode {
         waitForStart();
 
         // --- LOOP STARTS HERE ---
-        for(int i = 0; i < 7; i++) {
+        for(int i = 0; i < 5; i++) {
             
             telemetry.addData("Loop Count", i + 1);
             telemetry.update();
@@ -122,18 +122,18 @@ public class fourMotors extends LinearOpMode {
             runtime.reset();
             // Wait for completion
             while (opModeIsActive() && (myMotor0.isBusy() || myMotor1.isBusy()) && runtime.seconds() < 6.0) {
-                if(Math.abs(myMotor0.getCurrentPosition() - UP_POSITION) < 100)
-                {
-                    LIFT_VEL = 150;
-                }
-                else
-                {
-                    LIFT_VEL = 600;
-                }
-                myMotor0.setVelocity(LIFT_VEL);
-                myMotor1.setVelocity((int) (LIFT_VEL * ratio_offset));
-                myMotor2.setVelocity((int) (LIFT_VEL * ratio_offset));
-                myMotor3.setVelocity(LIFT_VEL);
+                // if(Math.abs(myMotor0.getCurrentPosition() - UP_POSITION) < 100)
+                // {
+                //     LIFT_VEL = 150;
+                // }
+                // else
+                // {
+                //     LIFT_VEL = 600;
+                // }
+                // myMotor0.setVelocity(LIFT_VEL);
+                // myMotor1.setVelocity((int) (LIFT_VEL * ratio_offset));
+                // myMotor2.setVelocity((int) (LIFT_VEL * ratio_offset));
+                // myMotor3.setVelocity(LIFT_VEL);
                     
                 telemetry.addData("Loop", i + 1);
                 telemetry.addData("Status", "Moving UP");
@@ -203,10 +203,10 @@ public class fourMotors extends LinearOpMode {
             myMotor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
             // tune these with loose collars
-            myMotor0.setPower(0.3);
-            myMotor1.setPower(0.3);
-            myMotor2.setPower(0.3);
-            myMotor3.setPower(0.3);
+            myMotor0.setPower(0.5);
+            myMotor1.setPower(0.5);
+            myMotor2.setPower(0.5);
+            myMotor3.setPower(0.5);
             
             telemetry.addData("Status", "pushing into bstop");
             telemetry.update();
@@ -220,7 +220,7 @@ public class fourMotors extends LinearOpMode {
             myMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             
             // decrements/increments
-            UP_POSITION -= 5;
+            // UP_POSITION -= 5;
             down_hold += 100;
     
         }
